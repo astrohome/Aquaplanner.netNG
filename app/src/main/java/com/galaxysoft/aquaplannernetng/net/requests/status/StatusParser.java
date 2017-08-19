e717ae5d3d59e33eec0d93d0e474a21305c3403e
Meka <meka4991@gmail.com>
2017-08-20 00:04:24 +0300

    Added StatusParser + some UTs.

--- app/src/main/java/com/galaxysoft/aquaplannernetng/net/requests/status/StatusParser.java
+++ app/src/main/java/com/galaxysoft/aquaplannernetng/net/requests/status/StatusParser.java
@@ -1,16 +1,60 @@
 package com.galaxysoft.aquaplannernetng.net.requests.status;
 
+import com.galaxysoft.aquaplannernetng.model.RelayState;
 import com.galaxysoft.aquaplannernetng.model.Status;
 import com.galaxysoft.aquaplannernetng.net.requests.base.BaseParser;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 
 public class StatusParser implements BaseParser<Status> {
     @NotNull
     @Override
+
+
     public List<Status> parse(@NotNull byte[] data) {
-        return null;
+
+        String line = new String(data, Charset.forName("UTF-8"));
+        String[] input = line.substring(1, line.length() - 1).split(",");
+        for (int i = 0; i < input.length; i++) {
+            input[i] = input[i].trim();
+        }
+
+        RelayState[] t = new RelayState[8];
+        for (int i = 3; i <= 10; i++) {
+            RelayState state;
+            int r = Integer.valueOf(input[i]);
+            if (r == 0) {
+                state = RelayState.FORCED_OFF;
+            } else if (r == 3) {
+                state = RelayState.FORCED_ON;
+            } else if (r == 2) {
+                state = RelayState.ON;
+            } else {
+                state = RelayState.OFF;
+            }
+
+            t[10 - i] = state;
+        }
+        int[] led = new int[6];
+        for (int i = 12; i <= 17; i++) {
+            int r = Integer.valueOf(input[i]);
+            if (r >= 0 && r <= 100) {
+                led[17 - i] = r;
+            } else {
+                led[17 - i] = -1;
+            }
+        }
+        //if(data[1]>-99; data[1])
+
+        Status resultat;
+        resultat = new Status(0f, 0f, 0f, null, led[0], led[1], led[2], led[3], led[4], led[5],
+                t[0], t[1], t[2], t[3], t[4], t[5], t[6], t[7], true);
+        List<Status> list = new ArrayList<Status>();
+        list.add(resultat);
+        return list;
     }
 }
--- app/src/test/java/com/galaxysoft/aquaplannernetng/StatusParserTest.java
+++ app/src/test/java/com/galaxysoft/aquaplannernetng/StatusParserTest.java
@@ -7,21 +7,30 @@ import com.galaxysoft.aquaplannernetng.net.requests.status.StatusParser;
 import junit.framework.Assert;
 
 import org.joda.time.LocalDateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 public class StatusParserTest {
 
     private StatusParser statusParser;
     private List<Status> result;
+    private List<Status> result2;
 
     @Before
-    public void setup() {
+    public void setup() throws UnsupportedEncodingException {
         statusParser = new StatusParser();
-        byte[] input = new byte[]{-99, 24, -99, 1, 3, 1, 1, 1, 1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 1, 21, 55, 14, 8, 17};
+
+        //                 ph    t1      t2 relay8 -----------    |  Fan led6 -------    |  Monday Hour  Minute  Day Month Year
+        byte[] input = "[-99.9, 26.6, -99.9, 1, 3, 1, 1, 1, 1, 0, 2,   0, 99, 120, 0, 0, 0, 15, 1,     21,   55,     14, 8,    17]".getBytes("UTF-8");
         result = statusParser.parse(input);
+
+        input = "[-99.9, 24.5, -99.9, 1, 0, 1, 1, 1, 1, 1, 2,   0, 9, 0, 0, 50, 1, 0, 1,     21,   55,     14, 8,    17]".getBytes("UTF-8");
+        result2 = statusParser.parse(input);
     }
 
     @Test
@@ -41,15 +50,43 @@ public class StatusParserTest {
     }
 
     @Test
+    public void testLed5() {
+        Status s = result.get(0);
+        Assert.assertEquals(-1, s.getLed5());
+        s = result2.get(0);
+        Assert.assertEquals(0, s.getLed5());
+    }
+
+    @Test
+    public void testLed3() {
+        Status s = result.get(0);
+        Assert.assertEquals(0, s.getLed3());
+        s = result2.get(0);
+        Assert.assertEquals(50, s.getLed3());
+    }
+
+    @Test
+    public void testLed6() {
+        Status s = result.get(0);
+        Assert.assertEquals(99, s.getLed6());
+        s = result2.get(0);
+        Assert.assertEquals(9, s.getLed6());
+    }
+
+
+    @Test
     public void testDate() {
         Status s = result.get(0);
-        Assert.assertEquals(LocalDateTime.parse("2017-08-14 21:55"), s.getDateTime());
+        DateTimeFormatter dtf = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm");
+        Assert.assertEquals(LocalDateTime.parse("2017-08-14 21:55", dtf), s.getDateTime());
     }
 
     @Test
     public void testRelay7() {
         Status s = result.get(0);
         Assert.assertEquals(RelayState.FORCED_ON, s.getRelay7());
+        Status s2 = result2.get(0);
+        Assert.assertEquals(RelayState.FORCED_OFF, s2.getRelay7());
     }
 
     @Test
@@ -68,5 +105,7 @@ public class StatusParserTest {
     public void testRelay2() {
         Status s = result.get(0);
         Assert.assertEquals(RelayState.FORCED_OFF, s.getRelay2());
+        Status s2 = result2.get(0);
+        Assert.assertEquals(RelayState.OFF, s2.getRelay2());
     }
 }
